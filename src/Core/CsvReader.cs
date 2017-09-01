8b759411c91930ff13b8a3f2fe4f7d60ac13a6eb
Miguel Roman <miguelerm@gmail.com>
2017-08-31 18:49:34 -0600

    add line details when an exception occurs

--- src/Core/CsvReader.cs
+++ src/Core/CsvReader.cs
@@ -7,53 +7,82 @@ namespace CsvToJson
 {
     public class CsvReader
     {
+        private int lineNumber;
         private string[] headers;
         private StreamReader input;
         private readonly string separator;
+        private string lastLine;
 
         public CsvReader(StreamReader input, string separator = ",")
         {
             this.input = input;
             this.separator = separator ?? ",";
+
+            lineNumber = 0;
         }
 
         public async Task<Dictionary<string, object>> ReadAsync()
         {
+            try
+            {
+                return await InternalReadAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new CsvReaderException($"Error on line {lineNumber}, last line: {lastLine}", ex);
+            }
+        }
+
+        private async Task<Dictionary<string, object>> InternalReadAsync()
+        {
             if (input.EndOfStream) return null;
 
-            var line = await input.ReadLineAsync();
-            var values = line?.Split(separator.ToCharArray()) ?? new string[]{};
+            lastLine = await ReadLine();
+            var values = lastLine?.Split(separator.ToCharArray()) ?? new string[] { };
 
-            if (headers == null) {
+            if (headers == null)
+            {
                 headers = values;
 
                 if (input.EndOfStream) return null;
 
-                line = await input.ReadLineAsync();
-                values = line?.Split(separator.ToCharArray()) ?? new string[]{};
+                lastLine = await ReadLine();
+                values = lastLine?.Split(separator.ToCharArray()) ?? new string[] { };
             }
 
             var obj = new Dictionary<string, object>();
 
             for (int i = 0; i < headers.Length; i++)
             {
-                if (values.Length >= i) {
+                if (values.Length >= i)
+                {
                     var val = values[i];
                     if (double.TryParse(val, out var numVal))
                     {
                         obj.Add(headers[i], numVal);
-                    } else if (bool.TryParse(val, out var boolVal)) {
+                    }
+                    else if (bool.TryParse(val, out var boolVal))
+                    {
                         obj.Add(headers[i], boolVal);
-                    } else {
+                    }
+                    else
+                    {
                         obj.Add(headers[i], val);
                     }
-                } else {
+                }
+                else
+                {
                     obj.Add(headers[i], null);
                 }
             }
 
             return obj;
+        }
 
+        private Task<string> ReadLine()
+        {
+            lineNumber++;
+            return input.ReadLineAsync();
         }
     }
 }
---
+++ src/Core/CsvReaderException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace CsvToJson
+{
+    [Serializable]
+    public class CsvReaderException : Exception
+    {
+        public CsvReaderException()
+        {
+        }
+
+        public CsvReaderException(string message) : base(message)
+        {
+        }
+
+        public CsvReaderException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected CsvReaderException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
